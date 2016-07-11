6dd645889429f62fd079defd8ce37a2b716f7d35
Sagedev User <romero1@un.org>
2016-07-11 09:16:52 +0200

    Added some new prompts

--- termcolors.sh
+++ termcolors.sh
@@ -24,29 +24,51 @@ HOSTNAME=$(hostname)
 USER="$USER"
 
 # Default prompt for unknown machines:
-if [ "${USER}" == "root" ]; then
+if [[ "${USER}" == "root" ]]; then
     export PS1="${L_RED}\u@\H:${BLUE} \w \$${NORMAL} "
 else
     export PS1="${L_GREEN}\u@\H:${BLUE} \w \$${NORMAL} "
 fi
 
 # Home's laptop:
-if [ "${HOSTNAME}" == "compiler" ];
+if [[ "${HOSTNAME}" == "compiler" ]];
 then
-    if [ "${USER}" == "sromero" ]; then
+    if [[ "${USER}" == "sromero" ]]; then
         export PS1="${L_GREEN}\u@\H:${BLUE} \w \$${NORMAL} "
-    elif [ "${USER}" == "root" ]; then
+    elif [[ "${USER}" == "root" ]]; then
         export PS1="${L_RED}\u@\H:${BLUE} \w \$${NORMAL} "
     fi
-fi
 
 # dfs-sgf01qa-82
-if [ "${HOSTNAME}" == "dfs-sgf01qa-82" ];
+elif [[ "${HOSTNAME}" == "dfs-sgf01qa-82" ]];
 then
-    if [ "${USER}" == "sromero" ]; then
+    if [[ "${USER}" == "sromero" ]] || [[ "${USER}" == "sagedev" ]]; then
         export PS1="${L_YELLOW}\u@\H:${BLUE} \w ${PURPLE}\$git_branch${RED}\$git_dirty ${GREEN}\$${NORMAL} "
-    elif [ "${USER}" == "root" ]; then
+    elif [[ "${USER}" == "root" ]]; then
         export PS1="${CYAN}(SAGE) ${L_RED}\u@\H:${BLUE} \w #${NORMAL} "
+    fi
+
+# dfs-sgb01qa-82
+elif [[ "${HOSTNAME}" == "dfs-sgb01qa-82" ]];
+then
+    if [[ "${USER}" == "root" ]]; then
+        export PS1="${CYAN}(SAGE) ${L_PURPLE}\u@\H:${BLUE} \w #${NORMAL} "
+    fi
+
+# dfs-dpf01qa-81
+elif [[ "${HOSTNAME}" == "dfs-dpf01qa-81" ]];
+then
+    if [[ "${USER}" == "drupal" ]]; then
+        export PS1="${L_YELLOW}\u@\H:${BLUE} \w \$${NORMAL} "
+    elif [[ "${USER}" == "root" ]]; then
+        export PS1="${L_GREEN}(DRP-${L_PURPLE}C7${L_GREEN}) ${L_RED}\u@\H:${BLUE} \w #${NORMAL} "
+    fi
+
+# dfs-dpb01qa-81
+elif [[ "${HOSTNAME}" == "dfs-dpb01qa-81" ]];
+then
+    if [[ "${USER}" == "root" ]]; then
+        export PS1="${L_WHITE}(${L_GREEN}DRP${L_WHITE}-${L_PURPLE}C7${L_WHITE}) ${L_RED}\u${L_BLUE}@${L_PURPLE}\H:${BLUE} \w #${NORMAL} "
     fi
 fi
 
