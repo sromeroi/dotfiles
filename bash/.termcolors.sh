57db25ff35814583e0fde0714bbcbb05cd4e699c
Santiago Romero <sromero@gmail.com>
2017-07-29 11:05:42 +0200

    (bash) Added yellow colour for 1 hostname and 2 users

--- bash/.termcolors.sh
+++ bash/.termcolors.sh
@@ -31,12 +31,15 @@ else
 fi
 
 # Home's laptop:
-if [[ "${HOSTNAME}" == "compiler" ]];
+if [[ "${HOSTNAME}" == "compiler" ]] || [[ "${HOSTNAME}" == "hpserver" ;
 then
     if [[ "${USER}" == "sromero" ]]; then
         export PS1="${L_GREEN}\u@\H:${BLUE} \w \$${NORMAL} "
     elif [[ "${USER}" == "root" ]]; then
         export PS1="${L_RED}\u@\H:${BLUE} \w \$${NORMAL} "
+    elif [[ "${USER}" == "drupal" ]] || [[ "${USER}" == "sage" ]]; then
+        export PS1="${L_YELLOW}\u@\H:${BLUE} \w \$${NORMAL} "
+        #export PS1="${L_YELLOW}\u@\H:${BLUE} \w ${PURPLE}\$git_branch${RED}\$git_dirty ${GREEN}\$${NORMAL} "
     fi
 
 # dfs-sgf01qa-82
